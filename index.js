5a3dcf4e7a7899391ed5633e9de77d81bf512a63
nhatmn2 <38064467+nhatmn2@users.noreply.github.com>
2021-11-20 17:31:12 -0500

    final commit to chattrbox

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ var fs = require('fs');
 var extract = require('./extract');
 var wss = require('./websockets-server');
 
-var handleError = function (err, res) {
+var handleError = function(err, res) {
     res.writeHead(404);
     res.end();
 };
 
-var server = http.createServer(function (req, res) {
+var server = http.createServer(function(req, res) {
     console.log('Responding to a request.');
-    var filepath = extract(req.url);
-    fs.readFile(filepath, function (err, data) {
+    var filePath = extract(req.url);
+    fs.readFile(filePath, function(err, data) {
         if (err) {
-            handleError(err, res);
-            return;
-        }
+        handleError(err, res);
+        return;
+    } else {
         res.end(data);
+    }
     });
 });
 server.listen(3000);
-
